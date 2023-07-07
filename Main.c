d403b07e464163d56adb4aac31ee991e09670c20
Anton Rahmaninov <95240106+arahmaninov@users.noreply.github.com>
2023-07-07 15:55:55 +0300

    Add main window

--- Main.c
+++ Main.c
@@ -1,13 +1,109 @@
 #include <stdio.h>
 
-int main(int argc, char* argv[])
+#pragma warning(push, 0)
+#include <windows.h>
+#pragma warning(pop)
+
+
+LRESULT CALLBACK MainWndowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(HINSTANCE Instance, HINSTANCE PreviousInstance,
+    PSTR CommandLine, int CmdShow)
 {
-	printf("Arguments: %i\n", argc);
+    //UNREFERENCED_PARAMETER(Instance);
+    UNREFERENCED_PARAMETER(PreviousInstance);
+    UNREFERENCED_PARAMETER(CommandLine);
+    UNREFERENCED_PARAMETER(CmdShow);
 
-	if (argc > 1)
-	{
-		printf("- First argument is %s", argv[1]);
-	}
 
-	return 0;
+    WNDCLASSEXA WindowClass = { 0 };
+    HWND WindowHandle;
+    //MSG Msg;
+
+    WindowClass.cbSize = sizeof(WNDCLASSEXA);
+    WindowClass.style = 0;
+    WindowClass.lpfnWndProc = MainWndowProc;
+    WindowClass.cbClsExtra = 0;
+    WindowClass.cbWndExtra = 0;
+    WindowClass.hInstance = Instance;
+    WindowClass.hIcon = LoadIconA(NULL, IDI_APPLICATION);
+    WindowClass.hCursor = LoadCursorA(NULL, IDC_ARROW);
+    WindowClass.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    WindowClass.lpszMenuName = NULL;
+    WindowClass.lpszClassName = "GAME_WINDOWCLASS";
+    WindowClass.hIconSm = LoadIconA(NULL, IDI_APPLICATION);
+
+    if (RegisterClassExA(&WindowClass) == 0)
+    {
+        MessageBox(NULL, "Window Registration Failed!", "Error!",
+            MB_ICONEXCLAMATION | MB_OK);
+        return 0;
+    }
+
+
+
+    WindowHandle = CreateWindowExA(
+        WS_EX_CLIENTEDGE,
+        WindowClass.lpszClassName,
+        "Sky Merchant",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, 240, 120,
+        NULL, NULL, Instance, NULL);
+
+    if (WindowHandle == NULL)
+    {
+        MessageBox(NULL, "Window Creation Failed!", "Error!",
+            MB_ICONEXCLAMATION | MB_OK);
+        return 0;
+    }
+
+
+    ShowWindow(WindowHandle, TRUE);
+
+
+    MSG Message = { 0 };
+
+    while (GetMessageA(&Message, NULL, 0, 0) > 0)
+    {
+        TranslateMessage(&Message);
+
+        DispatchMessageA(&Message);
+    }
+
+    return 0;
+}
+
+LRESULT CALLBACK MainWndowProc(
+    HWND hwnd,        // handle to window
+    UINT uMsg,        // message identifier
+    WPARAM wParam,    // first message parameter
+    LPARAM lParam)    // second message parameter
+{
+
+    switch (uMsg)
+    {
+    case WM_CREATE:
+        // Initialize the window. 
+        return 0;
+
+    case WM_PAINT:
+        // Paint the window's client area. 
+        return 0;
+
+    case WM_SIZE:
+        // Set the size and position of the window. 
+        return 0;
+
+    case WM_DESTROY:
+        // Clean up window-specific data objects. 
+        return 0;
+
+        // 
+        // Process other messages. 
+        // 
+
+    default:
+        return DefWindowProc(hwnd, uMsg, wParam, lParam);
+    }
+    return 0;
 }
